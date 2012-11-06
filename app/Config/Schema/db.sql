af919e8eb589e965dacf4a082e2972d7abd305a0
Amir Shevat <amir.shevat@gmail.com>
2012-11-06 20:18:16 +0200

    back to nimo version

--- app/Config/Schema/db.sql
+++ app/Config/Schema/db.sql
@@ -497,6 +497,24 @@ INSERT INTO `user_settings` (`id`, `name`, `name_public`, `value`, `type`) VALUE
 
 
 
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 09, 2012 at 09:27 AM
+-- Server version: 5.5.24-log
+-- PHP Version: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
 -- Database: `xboard`
 --
